d57f889900c9734492a265c89439d05bf4265679
casperlee <casperlee@thinkgeo.com>
2018-05-17 16:40:41 +0800

    Add Routing sample for iOS

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# RoutingSample-ForiOS
+# RoutingSample For iOS
+
+### Description
+
+
 The Map Suite Routing “How Do I?” solution offers a series of useful how-to examples for using the Map Suite Routing extension. The bundled solution comes with a small set of routable street data from Newfoundland Island Realty, Canada and demonstrates simple routing, avoiding specific areas, getting turn-by-turn directions, optimizing for the Traveling Salesman Problem, and much more. Full source code is included in both C# and VB.NET languages; simply select your preferred language to download the associated solution.
+
+Please refer to [Wiki](http://wiki.thinkgeo.com/wiki/map_suite_mobile_for_iOS) for the details.
+
+![Screenshot](https://github.com/ThinkGeo/RoutingSample-ForiOS/blob/master/ScreenShot.png)
+
+### Requirements
+This sample makes use of the following NuGet Packages
+
+[MapSuite 10.0.0](https://www.nuget.org/packages?q=ThinkGeo)
+
+[ThinkGeo.MapSuite.Layers.ShapeFile](https://www.nuget.org/packages/ThinkGeo.MapSuite.Layers.ShapeFile/11.0.0-beta008)
+
+[MapSuiteMobileForiOS-BareBone](https://www.nuget.org/packages/MapSuiteMobileForiOS-BareBone/11.0.0-beta037)
+
+[ThinkGeo.MapSuite.Routing](https://www.nuget.org/packages/ThinkGeo.MapSuite.Routing/11.0.0-beta012)
+
+### About the Code
+```csharp
+string rtgPath = Path.Combine("AppData", "land_lnd_street_segment_route.rtg");
+var routingSource = new RtgRoutingSource(rtgPath);
+var featureSource = new ShapeFileFeatureSource(shapeFilePath);
+featureSource.Open();
+var boundry = featureSource.GetBoundingBox();
+routingEngine = new RoutingEngine(routingSource, featureSource);
+routingEngine.GeographyUnit = GeographyUnit.Meter;
+routingEngine.SearchRadiusInMeters = 2000000;
+```
+### Getting Help
+
+[Map Suite Web for iOS Wiki Resources](http://wiki.thinkgeo.com/wiki/map_suite_mobile_for_iOS)
+
+[Map Suite Web for iOS Product Description](https://thinkgeo.com/mobile)
+
+[ThinkGeo Community Site](http://community.thinkgeo.com/)
+
+[ThinkGeo Web Site](http://www.thinkgeo.com)
+
+### Key APIs
+This example makes use of the following APIs:
+
+- [ThinkGeo.MapSuite.Routing.RtgRoutingSource](http://wiki.thinkgeo.com/wiki/api/thinkgeo.mapsuite.routing.rtgroutingsource)
+- [ThinkGeo.MapSuite.Layers.ShapeFileFeatureSource](http://wiki.thinkgeo.com/wiki/api/thinkgeo.mapsuite.layers.shapefilefeaturesource)
+- [ThinkGeo.MapSuite.Routing.RoutingEngine](http://wiki.thinkgeo.com/wiki/api/thinkgeo.mapsuite.routing.routingengine)
+- [ThinkGeo.MapSuite.Shapes.RectangleShape](http://wiki.thinkgeo.com/wiki/api/thinkgeo.mapsuite.shapes.rectangleshape)
+
+### About Map Suite
+Map Suite is a set of powerful development components and services for the .Net Framework.
+
+### About ThinkGeo
+ThinkGeo is a GIS (Geographic Information Systems) company founded in 2004 and located in Frisco, TX. Our clients are in more than 40 industries including agriculture, energy, transportation, government, engineering, software development, and defense.
---
+++ RoutingSampleiOS/ViewController.cs
@@ -0,0 +1,95 @@
+﻿using CoreGraphics;
+using System;
+using System.IO;
+using ThinkGeo.MapSuite;
+using ThinkGeo.MapSuite.Drawing;
+using ThinkGeo.MapSuite.iOS;
+using ThinkGeo.MapSuite.Layers;
+using ThinkGeo.MapSuite.Routing;
+using ThinkGeo.MapSuite.Shapes;
+using ThinkGeo.MapSuite.Styles;
+using UIKit;
+
+namespace RoutingSample_iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        private MapView mapView;
+        private LayerOverlay layerOverlay;
+        private RoutingLayer routingLayer;
+        private RoutingEngine routingEngine;
+        private bool firstClick = true;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            mapView = new MapView(View.Frame);
+            View.Add(mapView);
+
+            mapView.MapUnit = GeographyUnit.Meter;
+            mapView.MapSingleTap += MapViewMapSingleTap;
+
+            LayerOverlay backgroundOverlay = new LayerOverlay();
+            string shapeFilePath = Path.Combine("AppData", "land_lnd_street_segment_route.routable.shp");
+            ShapeFileFeatureLayer shapeFileFeatureLayer = new ShapeFileFeatureLayer(shapeFilePath);
+            shapeFileFeatureLayer.ZoomLevelSet.ZoomLevel01.DefaultLineStyle = WorldStreetsLineStyles.MotorwayFill(2.0f);
+            shapeFileFeatureLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+            backgroundOverlay.Layers.Add(shapeFileFeatureLayer);
+            mapView.Overlays.Add(backgroundOverlay);
+
+            layerOverlay = new LayerOverlay();
+            routingLayer = new RoutingLayer();
+            layerOverlay.Layers.Add(routingLayer);
+            mapView.Overlays.Add(layerOverlay);
+
+            string rtgPath = Path.Combine("AppData", "land_lnd_street_segment_route.rtg");
+            var routingSource = new RtgRoutingSource(rtgPath);
+            var featureSource = new ShapeFileFeatureSource(shapeFilePath);
+            featureSource.Open();
+            var boundry = featureSource.GetBoundingBox();
+            routingEngine = new RoutingEngine(routingSource, featureSource);
+            routingEngine.GeographyUnit = GeographyUnit.Meter;
+            routingEngine.SearchRadiusInMeters = 2000000;
+
+            mapView.CurrentExtent = new RectangleShape(307619.124092, 900219997.607509, 121246866.13256, 800097784.115004);
+            mapView.Refresh();
+        }
+
+        private void MapViewMapSingleTap(object sender, UIGestureRecognizer e)
+        {
+            CGPoint location = e.LocationInView(View);
+            PointShape position = ExtentHelper.ToWorldCoordinate(mapView.CurrentExtent, (float)location.X, (float)location.Y, (float)View.Frame.Width, (float)View.Frame.Height);
+
+            routingLayer.Routes.Clear();
+            if (firstClick)
+            {
+                routingLayer.StartPoint = position;
+                firstClick = false;
+            }
+            else
+            {
+                routingLayer.EndPoint = position;
+                firstClick = true;
+            }
+
+            routingLayer.Routes.Clear();
+            if (routingLayer.StartPoint != null && routingLayer.EndPoint != null)
+            {
+                RoutingResult routingResult = routingEngine.GetRoute(routingLayer.StartPoint, routingLayer.EndPoint);
+                routingLayer.Routes.Add(routingResult.Route);
+            }
+
+            layerOverlay.Refresh();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
